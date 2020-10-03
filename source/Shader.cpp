d4e2ef8e846282aba2248ff30c9ddb83e2075da5
Denmey <Denmey98@gmail.com>
2020-10-03 10:16:34 +0300

    Add automatic version change based on target platform

--- source/Shader.cpp
+++ source/Shader.cpp
@@ -22,7 +22,7 @@ Shader::Shader(const std::string &vPath, const std::string &fPath)
 		this->fSource = *opt;
 	}
 
-	// std::cout << "Shader was successfully loaded.\n";
+	// std::cout << "   Shader was successfully loaded.\n";
 
 }
 
@@ -47,18 +47,35 @@ std::optional<std::string> Shader::loadFromFile(const std::string &path)
 		std::string original{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
 		const auto versionBegin    = original.find("#version");
 		const auto versionEnd      = original.find('\n', versionBegin);
-		const auto versionLen      = versionEnd-versionBegin+2;
-		std::cout << original << "\n";
-		const auto isCore          = original.find("core",      versionBegin, versionLen);
-		const auto isEs            = original.find("es",        versionBegin, versionLen);
-		const auto hasPrecision    = (original.find("precision", versionBegin, versionLen));
-		std::cout << original.substr(versionBegin, versionLen);
-		std::cout << versionBegin << ":" << versionEnd << "\n";
-		std::cout << isCore << ":" << isEs << ":" << hasPrecision << ":" << std::string::npos << "\n";
+		const auto versionLen      = versionEnd-versionBegin+1;
+		const auto isCore          = original.substr(versionBegin, versionLen).find("core") != std::string::npos;
+		const auto isEs            = original.substr(versionBegin, versionLen).find("es") != std::string::npos;
+		const auto hasPrecision    = original.find("\nprecision ") != std::string::npos;
+		// std::cout << original.substr(versionBegin, versionLen);
+		// std::cout << versionBegin << ":" << versionEnd << "\n";
+		// std::cout << isCore << ":" << isEs << ":" << hasPrecision << ":" << std::string::npos << "\n";
+		auto result = original;
+#ifdef __EMSCRIPTEN__
+		if (isCore) {
+			std::cout << "   Warning: replacing core version with 300 es.\n";
+			result.replace(versionBegin, versionEnd, esVersionLine);
+		}
+		if (!hasPrecision) {
+			std::cout << "   Warning: adding mediump precision.\n";
+			result.insert(versionEnd, precisionLine);
+		}
+#else
+		if (isEs) {
+			std::cout << "   Warning: replacing es version with 330 core.\n";
+			result.replace(versionBegin, versionEnd, coreVersionLine);
+		}
+#endif
 		file.close();
-		return std::string{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+		// std::cout << result << "\n";
+
+		return result;
 	} else {
-		std::cout << "Error: Couldn't load shader from '" + path + "'.\n";
+		std::cout << "   Error: Couldn't load shader from '" + path + "': check if file exists.\n";
 		return std::nullopt;
 	}
 	return std::nullopt;
